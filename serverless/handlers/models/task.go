1d0e2a69d820ed9a0e1b90b9b7e539f1e5288bf7
olefirenque <egor.olefirenko892@gmail.com>
2021-11-04 11:51:56 +0300

    update task model

--- serverless/handlers/models/task.go
+++ serverless/handlers/models/task.go
@@ -1,11 +1,9 @@
 package models
 
 type Task struct {
-	LabId string `dynamo:"labid"`
-	Num   int    `dynamo:"num"`
-	Name  string `dynamo:"name"`
-
-	//Lang      string `dynamo:"lang"`
-	//Validator string `dynamo:"validator"`
-	//Generator string `dynamo:"generator"`
+	LabId     string `dynamo:"labid"`
+	Num       int    `dynamo:"num"`
+	Name      string `dynamo:"name"`
+	Validator string `dynamo:"validator"`
+	Generator string `dynamo:"generator"`
 }
