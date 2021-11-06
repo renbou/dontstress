240e45b450bd28b97a285511c3f53e0f56df3c93
olefirenque <egor.olefirenko892@gmail.com>
2021-11-06 23:57:09 +0300

    feat: add DTOs

--- internal/dto/rundto.go
+++ internal/dto/rundto.go
@@ -1,12 +1,6 @@
-package models
+package dto
 
-import "github.com/renbou/dontstress/internal/dto"
-
-type Run struct {
-	Id     string `dynamo:"id" json:"id"`
-	Labid  string `dynamo:"labid" json:"labid"`
-	Taskid int    `dynamo:"taskid" json:"taskid"`
-	Fileid string `dynamo:"fileid" json:"fileid"`
+type RunDTO struct {
 	Status string `dynamo:"status" json:"status"`
 	Tests  []struct {
 		Result  string `dynamo:"results" json:"result"`
@@ -17,8 +11,4 @@ type Run struct {
 			Got      string `dynamo:"got" json:"got"`
 		} `dynamo:"info" json:"info"`
 	} `dynamo:"tests" json:"tests"`
-}
-
-func (run *Run) ToDTO() *dto.RunDTO {
-	return &dto.RunDTO{Status: run.Status, Tests: run.Tests}
 }
